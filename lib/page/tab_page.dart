2f74efcc4a97e375ca1aa55f9293db38da76edf2
artiper <somadubel@gmail.com>
2020-09-29 23:33:33 +0900

    BottomTapBar 1

--- lib/page/tab_page.dart
+++ lib/page/tab_page.dart
@@ -6,12 +6,35 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Search',
+    ),
+    Text(
+      'Index 2: Account',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
     home: Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+
       bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             title: Text('Home')
@@ -25,7 +48,10 @@ class _TabPageState extends State<TabPage> {
             title: Text('Account')
           ),
         ],
-      )
+        currentIndex: _selectedIndex,
+        // selectedItemColor: ,
+        onTap: _onItemTapped, //Called when one of the items is tapped.
+      ),
       )
     );
   }
